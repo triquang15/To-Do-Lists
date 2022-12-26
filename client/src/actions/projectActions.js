8311988099e8c8c11b448618c20b0223dc0df562
Nguyen <quangtri.nguyen@dxc.com>
2022-12-26 16:20:33 +0700

    Handle Errors Update

--- client/src/actions/projectActions.js
+++ client/src/actions/projectActions.js
@@ -4,7 +4,11 @@ import { GET_ERRORS, GET_PROJECT, GET_PROJECTS } from "./type";
 export const createProject = (project, history) => async dispatch => {
     try{
         const res = await axios.post("http://localhost:8081/api/project", project)
-        history.push("/dashboard")
+        history.push("/dashboard");
+        dispatch({
+            type:GET_ERRORS,
+            payload:{}
+        })
     } catch(err) {
         dispatch({
             type:GET_ERRORS,
@@ -22,9 +26,14 @@ export const getProjects = () => async dispatch => {
 }
 
 export const getProject = (id, history) => async dispatch => {
-    const res = await axios.get(`http://localhost:8081/api/project/${id}`)
-    dispatch({
-        type: GET_PROJECT,
-        payload: res.data
-    });
+    try {
+        const res = await axios.get(`http://localhost:8081/api/project/${id}`)
+        dispatch({
+            type: GET_PROJECT,
+            payload: res.data
+        });
+    } catch (error) {
+        history.push("/dashboard");
+    }
+   
 }
--- client/src/components/Project/UpdateProject.js
+++ client/src/components/Project/UpdateProject.js
@@ -3,7 +3,6 @@ import { getProject, createProject } from "../../actions/projectActions";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import classnames from "classnames";
-
 class UpdateProject extends Component {
   //set state
   constructor() {
@@ -15,13 +14,17 @@ class UpdateProject extends Component {
       projectIdentifier: "",
       description: "",
       startDate: "",
-      endDate: ""
+      endDate: "",
+      errors: {}
     };
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
+    if(nextProps.errors) {
+      this.setState({errors: nextProps.errors});
+    }
     const {
       id,
       projectName,
@@ -66,6 +69,7 @@ class UpdateProject extends Component {
   }
 
   render() {
+    const {errors} = this.state;
     return (
       <div className="project">
         <div className="container">
@@ -77,12 +81,19 @@ class UpdateProject extends Component {
                 <div className="form-group">
                   <input
                     type="text"
-                    className="form-control form-control-lg "
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.projectName
+                    })}
                     placeholder="Project Name"
                     name="projectName"
                     value={this.state.projectName}
                     onChange={this.onChange}
                   />
+                  {
+                    errors.projectName && (
+                      <div className="invalid-feedback">{errors.projectName}</div>
+                    )
+                  }
                 </div>
                 <div className="form-group">
                   <input
@@ -97,12 +108,19 @@ class UpdateProject extends Component {
                 </div>
                 <div className="form-group">
                   <textarea
-                    className="form-control form-control-lg"
+                   className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.description
+                    })}
                     placeholder="Project Description"
                     name="description"
                     onChange={this.onChange}
                     value={this.state.description}
                   />
+                  {
+                    errors.description && (
+                      <div className="invalid-feedback">{errors.description}</div>
+                    )
+                  }
                 </div>
                 <h6>Start Date</h6>
                 <div className="form-group">
@@ -141,11 +159,13 @@ class UpdateProject extends Component {
 UpdateProject.propTypes = {
   getProject: PropTypes.func.isRequired,
   createProject: PropTypes.func.isRequired,
-  project: PropTypes.object.isRequired
+  project: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = state => ({
-  project: state.project.project
+  project: state.project.project,
+  errors: state.errors
 });
 
 export default connect(
