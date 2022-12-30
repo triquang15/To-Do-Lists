fe4007639d11051ab293363c7c917162ffabfbbb
Nguyen <quangtri.nguyen@dxc.com>
2022-12-30 10:11:21 +0700

    Spring Security Config

---
+++ API/src/main/java/com/devteam/entity/User.java
@@ -0,0 +1,5 @@
+package com.devteam.entity;
+
+public class User {
+
+}
---
+++ API/src/main/java/com/devteam/security/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.devteam.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
+public class SecurityConfig {
+	
+	@Autowired
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		 http.cors().and().csrf().disable()
+         .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+         .sessionManagement()
+         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+         .and()
+         .authorizeRequests()
+         .antMatchers(
+                 "/",
+                 "/favicon.ico",
+                 "/**/*.png",
+                 "/**/*.gif",
+                 "/**/*.svg",
+                 "/**/*.jpg",
+                 "/**/*.html",
+                 "/**/*.css",
+                 "/**/*.js"
+         ).permitAll()
+         .anyRequest().authenticated();
+		  http.headers().frameOptions().sameOrigin();
+		return http.build();
+	}
+
+}
