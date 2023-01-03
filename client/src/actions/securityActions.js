b01a2f95b440dd5c65f6efbd1960a995ea7165ee
Nguyen <quangtri.nguyen@dxc.com>
2023-01-03 11:36:58 +0700

    Code Fix

--- client/src/actions/securityActions.js
+++ client/src/actions/securityActions.js
@@ -3,38 +3,47 @@ import setJwtToken from "../security/setJwtToken";
 import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 import jwt_decode from "jwt-decode";
 
-export const createNewUser = (newUser, history)=> async dispatch => {
-    try {
-        await axios.post("/api/users/register", newUser);
-        history.push("/login");
-        dispatch({
-            type: GET_ERRORS,
-            payload: {}
-        });
-    } catch (error) {
-        dispatch({
-            type: GET_ERRORS,
-            payload: error.response.data
-        });
-    }
+export const createNewUser = (newUser, history) => async (dispatch) => {
+  try {
+    await axios.post("/api/users/register", newUser);
+    history.push("/login");
+    dispatch({
+      type: GET_ERRORS,
+      payload: {},
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data,
+    });
+  }
 };
 
-export const login = LoginRequest => async dispatch => {
-    try {
-        const res = await axios.post("/api/users/login", LoginRequest);
-        const {token} = res.data;
-        localStorage.setItem("jwtToken", token);
-        setJwtToken(token);
-        const decoded = jwt_decode(token);
+export const login = (LoginRequest) => async (dispatch) => {
+  try {
+    const res = await axios.post("/api/users/login", LoginRequest);
+    const { token } = res.data;
+    localStorage.setItem("jwtToken", token);
+    setJwtToken(token);
+    const decoded = jwt_decode(token);
 
-        dispatch({
-            type: SET_CURRENT_USER,
-            payload: decoded
-        })
-    } catch (error) {
-        dispatch({
-            type: GET_ERRORS,
-            payload: error.response.data
-        })
-    }
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: decoded,
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data,
+    });
+  }
+};
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("jwtToken");
+  setJwtToken(false);
+  dispatch({
+    type: SET_CURRENT_USER,
+    payload: {},
+  });
 };
--- client/src/components/Users/Login.js
+++ client/src/components/Users/Login.js
@@ -5,56 +5,93 @@ import { connect } from "react-redux";
 import classnames from "classnames";
 
 class Login extends Component {
-
   constructor() {
     super();
     this.state = {
       username: "",
-      password: ""
+      password: "",
+      errors: {},
     };
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentDidMount() {
+    if(this.props.security.validToken) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
   onChange(e) {
-    this.setState({[e.target.name]:e.target.value})
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.security.validToken) {
+      this.props.history.push("/dashboard");
+    }
+
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
   }
 
   onSubmit(e) {
     e.preventDefault();
     const loginRequest = {
       username: this.state.username,
-      password: this.state.password
+      password: this.state.password,
     };
 
     this.props.login(loginRequest);
   }
 
   render() {
+    const { errors } = this.state;
     return (
       <div className="login">
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
-              <h2 className="display-4 text-center"><b>Log in - Integral Suite JIRA</b></h2> <br></br>
+              <h2 className="display-4 text-center">
+                <b>Log in - Integral Suite JIRA</b>
+              </h2>{" "}
+              <br></br>
               <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
                     type="email"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.username,
+                    })}
                     placeholder="Email Address"
-                    name="username" value={this.state.username} onChange={this.onChange}
+                    name="username"
+                    value={this.state.username}
+                    onChange={this.onChange}
                   />
+                  {errors.username && (
+                    <div className="invalid-feedback">{errors.username}</div>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.password,
+                    })}
                     placeholder="Password"
-                    name="password" value={this.state.password} onChange={this.onChange}
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
                   />
-<br></br>
-                  <p>Not a member? To request an account, please contact your Jira administrators.</p>
+                  {errors.password && (
+                    <div className="invalid-feedback">{errors.password}</div>
+                  )}
+                  <br></br>
+                  <p>
+                    Not a member? To request an account, please contact your
+                    Jira administrators.
+                  </p>
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
@@ -68,12 +105,13 @@ class Login extends Component {
 
 Login.propTypes = {
   login: PropTypes.func.isRequired,
-  errors: PropTypes.object.isRequired
+  errors: PropTypes.object.isRequired,
+  security: PropTypes.object.isRequired,
 };
 
-const mapState = state => ({
+const mapState = (state) => ({
   security: state.security,
-  errors: state.errors
+  errors: state.errors,
 });
 
-export default connect(mapState, {login}) (Login);
+export default connect(mapState, { login })(Login);
